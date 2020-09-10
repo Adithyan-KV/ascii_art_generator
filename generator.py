77ceb81fb9965a084b4d180921d8646114c758ef
Adithyan-KV <adithyankv007@gmail.com>
2020-09-10 20:47:59 +0530

    Added conversion to ASCII and write out to file
     -The image can now be converted to ascii values
     -The values are being written out to a text file

--- generator.py
+++ generator.py
@@ -4,7 +4,9 @@ import numpy as np
 
 def main():
     image = Image.open('image.jpg')
-    processed_image = preprocess(image)
+    pre_processed_data = preprocess(image)
+    ascii_data = convert_to_ascii(pre_processed_data)
+    write_to_file(ascii_data)
 
 
 def preprocess(image):
@@ -15,7 +17,7 @@ def preprocess(image):
         image (PIL image): The image to be preprocessed
 
     Returns:
-        PIL image: The resized, greyscale image
+        nparray: The resized, greyscale image data matrix
     """
     width, height = image.size
     image = image.resize((width//10, height//10))
@@ -24,9 +26,35 @@ def preprocess(image):
     # https://en.wikipedia.org/wiki/Luma_(video)
     weight_array = np.array([0.3, 0.59, 0.11])
     luma_value_data = np.matmul(data, weight_array)
-    luma_value_image = Image.fromarray(luma_value_data)
-    luma_value_image.show()
-    return luma_value_image
+    return luma_value_data
+
+
+def convert_to_ascii(image_data):
+    # sorted from light to dark (assuming light background)
+    # character set selected from http://paulbourke.net/dataformats/asciiart/
+    characters_used = [' ', '.', ':', ';', '+', '?', '#', '%', '@', '$']
+    height, width = np.shape(image_data)
+    ascii_data = np.zeros_like(image_data, dtype=str)
+    for i in range(height):
+        for j in range(width):
+            value = normalize(image_data[i, j], 0, 9)
+            ascii_data[i, j] = characters_used[value]
+    print(ascii_data)
+    return ascii_data
+
+
+def normalize(value, lower_bound, upper_bound):
+    normalized_value = round(value/255*(upper_bound-lower_bound))
+    return normalized_value
+
+
+def write_to_file(data):
+    height, width = np.shape(data)
+    with open('test.txt', 'w') as fopen:
+        for i in range(height):
+            for j in range(width):
+                fopen.write(data[i, j])
+            fopen.write('\n')
 
 
 if __name__ == "__main__":
