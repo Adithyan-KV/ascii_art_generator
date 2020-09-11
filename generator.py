19e0557a36b244d4d73b1fbaee6d484da0d69d7e
Adithyan-KV <adithyankv007@gmail.com>
2020-09-11 22:48:59 +0530

    Added custom output file dimensions
     -The output ASCII files can now be of custom dimensions
     -Options were added to
       +Keep aspect ratio maintaining height same
       +Keep aspect ratio maintaining width same
       +Keep original dimensions of input image
       +Input custom height and width
       +By default maintains the original dimensions of input image

--- generator.py
+++ generator.py
@@ -6,23 +6,24 @@ import sys
 
 class Converter():
 
-    def __init__(self, *args, **kwargs):
-        path = sys.argv[1]
+    def __init__(self, args):
+        path = args[1]
+        dimension_args = args[2:]
         if os.path.exists(path):
             self.filepath = path
             self.basename = os.path.basename
         else:
             raise OSError(f"File not found: the file {path} couldn't be found")
-        self.generate_ascii_from_image(self.filepath)
 
-    def generate_ascii_from_image(self, filename, *args, **kwargs):
-        filename = sys.argv[1]
+        self.generate_ascii_from_image(self.filepath, dimension_args)
+
+    def generate_ascii_from_image(self, filename, dimension_args):
         image = Image.open(f'{filename}')
-        pre_processed_data = self.preprocess(image)
+        pre_processed_data = self.preprocess(image, dimension_args)
         ascii_data = self.convert_to_ascii(pre_processed_data)
-        self.write_to_file(ascii_data, f'{filename}')
+        self.write_to_file(ascii_data, f'{os.path.splitext(filename)[0]}')
 
-    def preprocess(self, image, *args):
+    def preprocess(self, image, dimension_args):
         """Preprocesses the image before being processed to convert to ascii. Image
         is resized and converted to greyscale values
 
@@ -32,10 +33,65 @@ class Converter():
         Returns:
             nparray: The resized, greyscale image data matrix
         """
-        if args:
-            print(len(args))
-        width, height = image.size
-        image = image.resize((width//10, height//20))
+        img_width, img_height = image.size
+        if dimension_args:
+            flag = dimension_args[0]
+            print(flag)
+            arg_number = len(dimension_args)
+            if not flag:
+                # defaults to original image size
+                flag = '-o'
+            if flag == '-h':
+                if arg_number != 2:
+                    if arg_number < 2:
+                        raise ValueError("Missing argument [height] after -h")
+                    else:
+                        raise ValueError(
+                            f"Too many arguments, one argument was expected after -h, but {arg_number} were provided")
+                else:
+                    try:
+                        output_height = int(dimension_args[1])
+                        output_width = round(
+                            output_height*(img_width/img_height))
+                    except Exception:
+                        raise TypeError("Height must be a number")
+            elif flag == '-w':
+                if arg_number != 2:
+                    if arg_number < 2:
+                        raise ValueError("Missing argument [height] after -h")
+                    else:
+                        raise ValueError(
+                            f"Too many arguments, one argument was expected after -w, but {arg_number} were provided")
+                else:
+                    try:
+                        output_width = int(dimension_args[1])
+                        output_height = round(
+                            output_width*(img_height/img_width))
+                    except Exception:
+                        raise TypeError("Height must be a number")
+            elif flag == '-o':
+                if arg_number != 1:
+                    raise ValueError(
+                        f"Too many arguments, No arguments expected after -o, but {arg_number} were provided")
+                output_width = img_width
+                output_height = img_height
+            elif flag == '-c':
+                if arg_number != 3:
+                    if arg_number < 3:
+                        raise ValueError(
+                            "Missing arguments after -h, two arguments [width],[height] expected")
+                    else:
+                        raise ValueError(
+                            f"Too many arguments, one argument was expected after -w, but {arg_number} were provided")
+                else:
+                    try:
+                        output_width = int(dimension_args[1])
+                        output_height = int(dimension_args[2])
+                    except Exception:
+                        raise TypeError("Height must be a number")
+            else:
+                raise ValueError(f'invalid flag "{flag}"')
+        image = image.resize((output_width, output_height))
         data = np.asarray(image)
         # weights chosen in accordance with
         # https://en.wikipedia.org/wiki/Luma_(video)
@@ -66,7 +122,7 @@ class Converter():
         return ascii_data
 
     def normalize(self, value, lower_bound, upper_bound):
-        """Normalize pixel values between 0-255 to a range between upper and lower 
+        """Normalize pixel values between 0-255 to a range between upper and lower
         bound values, so as to correspond to number of ASCII intensity values used
 
         Args:
